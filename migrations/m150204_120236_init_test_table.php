38ef7295975357822f947d70cbf0fe8b8d915744
tessierashpool <rashidov_r@list.ru>
2015-03-03 20:55:28 +0400

    Market on Yii2

---
+++ migrations/m150204_120236_init_test_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150204_120236_init_test_table extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m150204_120236_init_test_table cannot be reverted.\n";
+
+        return false;
+    }
+}
